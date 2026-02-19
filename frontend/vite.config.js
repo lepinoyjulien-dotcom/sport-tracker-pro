43e246a99e161ad61d66b146844aa5a06a46c6cf
Julio Lep <lepinoy.julien@gmail.com>
2026-02-19 12:47:07 +0100

    Force new bundle hash generation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,14 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+        manualChunks: undefined
+      }
+    }
+  }
 })
